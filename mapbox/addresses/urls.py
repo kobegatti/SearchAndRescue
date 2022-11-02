af25f8ab7f7fe25594e7448d3a4411dd649f716a
kobegatti <kobegatti@gmail.com>
2022-11-02 15:59:12 -0700

    marker and data processing mechanics

--- mapbox/addresses/urls.py
+++ mapbox/addresses/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import AddressView
+from .views import AddressView, some_func
 
 urlpatterns = [
-   path('', AddressView.as_view(), name='home')
+   path('', AddressView.as_view(), name='home'),
+   path('random_url/', some_func)
 ]
--- mapbox/addresses/views.py
+++ mapbox/addresses/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from .models import Address
+from django.http import HttpResponse
+import json
 
 # Create your views here.
 
@@ -18,3 +20,20 @@ class AddressView(CreateView):
         context['mapbox_access_token'] = 'pk.eyJ1Ijoia2dhdHRpIiwiYSI6ImNsOTNjbmU2ZzF0NzYzbm1mdXBzcWl2eXgifQ.5e8SPJh6Q7-eCfX3vI42EA'
         context['addresses'] = Address.objects.all()
         return context
+
+def some_func(request):
+    if request.method == 'GET':
+        param1 = request.GET.get('param_first')
+        param2 = request.GET.get('param_second')
+
+        response_data = 'success!'
+
+        return HttpResponse(
+            json.dumps({"latitude": param1, "longitude": param2}),
+            content_type="appliction/json"
+        )
+    else:
+        return HttpResponse(
+            json.dumps({"nothing to see": "yeet"}),
+            content_type="application/json"
+        )
